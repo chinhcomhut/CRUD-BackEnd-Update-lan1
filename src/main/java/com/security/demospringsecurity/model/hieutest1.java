e8234709853c344f00d6e35df824ac1bf38cc0d5
DoanHieu <doanhieu.bl.hn@gmail.com>
2019-12-11 12:42:56 +0700

    tesk

---
+++ src/main/java/com/security/demospringsecurity/model/hieutest1.java
@@ -0,0 +1,22 @@
+package com.security.demospringsecurity.model;
+
+public class hieutest1 {
+    private String Doanhieu;
+    private String  test;
+
+    public String getDoanhieu() {
+        return Doanhieu;
+    }
+
+    public void setDoanhieu(String doanhieu) {
+        Doanhieu = doanhieu;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
